82e064a2f46fcc67d90f2888c9be77a5a3abfb09
aishwary11 <aishwary46@gmail.com>
2023-05-07 17:47:20 +0530

    rocket icon added

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.get('/', async (req: Request, res: Response) => {
   // const user: Users[] = await poolQuery("SELECT id, address, name, to_char(created_at, 'DD-MM-YYYY HH24:mm') as created_at FROM users");
   // return res.status(200).json({ user, ts: new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(new Date()) });
 });
-app.listen(port, () => console.log(`Server starting at ${port}`));
+app.listen(port, () => console.log(`Server starting at ${port} 🚀`));
