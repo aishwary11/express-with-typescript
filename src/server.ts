7b7c2b40a7043077d685e6d53f3e2867746fe282
aishwary11 <aishwary46@gmail.com>
2023-05-07 23:37:02 +0530

    dotenv added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
+import * as dotenv from 'dotenv';
 import express, { Express, Request, Response } from "express";
+import { poolQuery } from "./db/config";
+import { Users } from "./types/types";
+dotenv.config();
 const app: Express = express();
 const port: number = 5000;
 app.get('/', async (req: Request, res: Response) => {
-  console.log('ts => ', new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(new Date()));
-  return res.status(200).json({ msg: 'Hello World!!', ts: new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(new Date()) });
   // const value: (string | number)[] = ['Aish' as string, 25 as number, 'India' as string];
   // const user = await poolQuery('INSERT INTO users(name, age, address) VALUES($1, $2, $3) Returning *', value);
-  // const user: Users[] = await poolQuery("SELECT id, address, name, to_char(created_at, 'DD-MM-YYYY HH24:mm') as created_at FROM users");
-  // return res.status(200).json({ user, ts: new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(new Date()) });
+  const user: Users[] = await poolQuery("SELECT id, address, name, to_char(created_at, 'DD-MM-YYYY HH24:mm') as created_at FROM users");
+  return res.status(200).json({ user, ts: new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'medium' }).format(new Date()) });
 });
 app.listen(port, () => console.log(`Server starting at ${port} 🚀`));
